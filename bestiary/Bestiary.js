adb7a3d98dbba7b6884352de1f523cd81a53b75e
Gen Heartz <genheartz.mat@gmail.com>
2026-05-30 17:55:46 +0100

    index

--- bestiary/Bestiary.js
+++ bestiary/Bestiary.js
@@ -14,7 +14,7 @@ var ROUTES = {
     'monsters-by-region': 'MonstersByRegion.html'
 };
 
-var HOME_PATH = '../home/Home.html';
+var HOME_PATH = '../home/index.html';
 
 function initNavigation() {
     document.querySelectorAll('.nav-btn[data-page]').forEach(function (btn) {
